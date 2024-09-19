eb2aea16509a81bc296d37523c7e5b66060e0603
iamnimix <nima.abarghooie.7255@gmail.com>
2024-09-19 20:01:36 +0330

    add other filters for handlers

--- core/main.py
+++ core/main.py
@@ -16,4 +16,22 @@ def send_welcome(message):
     bot.send_message(message.chat.id, json.dumps(message.chat.__dict__, indent=1, ensure_ascii=False))
 
 
+@bot.message_handler(content_types=['document', 'audio'])
+def check_doc_audio(message):
+    if message.content_type == 'document':
+        bot.reply_to(message, "it's a document")
+    elif message.content_type == 'audio':
+        bot.reply_to(message, "it's an audio")
+
+
+@bot.message_handler(regexp=r'^Hello$')
+def say_hello(message):
+    bot.reply_to(message, 'Hello :)')
+
+
+@bot.message_handler(func=lambda message: message.content_type == 'text')
+def handle_text(message):
+    bot.send_message(message.chat.id, message.chat.username)
+
+
 bot.infinity_polling()
